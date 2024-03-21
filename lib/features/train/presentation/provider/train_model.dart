5878cf2eaaecb8c247d4ab6dc77994d49975d189
Max <maxspeedcubezzz@gmail.com>
2024-03-21 19:06:00 +0700

    downloading payload

--- lib/features/train/presentation/provider/train_model.dart
+++ lib/features/train/presentation/provider/train_model.dart
@@ -1,13 +1,17 @@
 import 'dart:async';
 import 'dart:developer';
+import 'dart:typed_data';
 
+import 'package:cbor/cbor.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:inmotion_mobile_test/core/utils/decoder/inmotion_tag_data_decoder.dart';
 import 'package:inmotion_mobile_test/features/train/data/data_sources/demo_resources.dart';
+import 'package:inmotion_mobile_test/features/train/domain/entities/measure_entity.dart';
 import 'package:inmotion_mobile_test/features/train/domain/entities/player_entity.dart';
 import 'package:inmotion_mobile_test/features/train/domain/entities/sensor_entity.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:typed_data/typed_buffers.dart';
 
 class TrainModel extends ChangeNotifier {
   final decoder = InmotionTagDataDecoder();
@@ -18,6 +22,9 @@ class TrainModel extends ChangeNotifier {
   TrainStage get trainStage => _trainStage;
 
   SystemStatus get systemStatus {
+    /// Если сейчас экран окончания тренировки, то SystemStatus.off
+    if (_trainStage == TrainStage.end) return SystemStatus.off;
+
     /// Если в _selectedPlayers есть неподключенные, то SystemStatus.error
     /// При любых обстоятельствах, тренировка это или нет, если есть неподключенные датчики то ошибка
     if (_selectedPlayers
@@ -55,11 +62,15 @@ class TrainModel extends ChangeNotifier {
   bool get isBLEOn => _bluetoothState == BluetoothAdapterState.on;
 
   // Model
+  double _loadingPercent = 0;
+
   bool _isTrainStart = false;
 
   final _players = <PlayerEntity>[];
 
   final _selectedPlayers = <PlayerEntity>[];
+
+  double get loadingPercent => _loadingPercent;
 
   List<PlayerEntity> get players => _players;
 
@@ -71,14 +82,14 @@ class TrainModel extends ChangeNotifier {
 
     /// Для идентификации датчиков
     /// Все датчики будут иметь этот Guid
-    final guid = Guid("243a0000-1234-2374-5673-a8a1593ef645");
+    final serviceGuid = Guid("243a0000-1234-2374-5673-a8a1593ef645");
 
     /// Поток работает всегда для прослушки имерений из adv пакетов
     _scanResultStream = FlutterBluePlus.onScanResults.listen(
       (results) {
         _foundedDevices.clear();
         for (final scanResult in results) {
-          if (scanResult.advertisementData.serviceUuids.contains(guid)) {
+          if (scanResult.advertisementData.serviceUuids.contains(serviceGuid)) {
             /// Девайс нашелся, но не добавлен
             if (!_players
                 .map((p) => p.sensor.device)
@@ -87,6 +98,7 @@ class TrainModel extends ChangeNotifier {
               notifyListeners();
               return;
             }
+
             /// Девайс уже добавлен, достаем данные
             final player = _players
                 .where((p) => p.sensor.device == scanResult.device)
@@ -161,9 +173,96 @@ class TrainModel extends ChangeNotifier {
   void stopRecording() {
     _isTrainStart = false;
     assert(_trainStage == TrainStage.running,
-        "TrainStage is not running. Can not start train");
+        "TrainStage is not running. Can not stop train");
     _trainStage = TrainStage.end;
+    _downloadDataFromDevices();
     notifyListeners();
+  }
+
+  void prepareRecording() {
+    assert(_trainStage == TrainStage.end,
+        "TrainStage is not stop. Can not prepare train");
+    _trainStage = TrainStage.prepare;
+    _startScanning();
+    notifyListeners();
+  }
+
+  void _downloadDataFromDevices() async {
+    log("start downloading");
+    _loadingPercent = 0;
+    notifyListeners();
+    // read char to get payload count
+    // char with uuid 243a0000-1234-2374-5673-a8a1593ef645
+    final serviceGuid = Guid("243a0000-1234-2374-5673-a8a1593ef645");
+    final charGuid = Guid("243a0003-1234-2374-5673-a8a1593ef645");
+
+    var totalLength = 0;
+    var downloaded = 0;
+
+    for (final player in selectedPlayers) {
+      final device = player.sensor.device;
+      await device.connect();
+      final service = (await device.discoverServices())
+          .firstWhere((service) => service.serviceUuid == serviceGuid);
+      final payloadChar = service.characteristics
+          .firstWhere((char) => char.characteristicUuid == charGuid);
+
+      List<int> value = await payloadChar.read();
+      final byteData = ByteData.sublistView(Uint8List.fromList(value));
+      final length = byteData.getInt32(0, Endian.little);
+      totalLength += length;
+    }
+
+    log("total length: ${totalLength}");
+
+    for (final player in selectedPlayers) {
+      final device = player.sensor.device;
+      //already connected
+      //await device.connect();
+      final service = (await device.discoverServices())
+          .firstWhere((service) => service.serviceUuid == serviceGuid);
+      final payloadChar = service.characteristics
+          .firstWhere((char) => char.characteristicUuid == charGuid);
+
+      payloadChar.setNotifyValue(true);
+
+      final measureList = <MeasureEntity>[];
+      StreamSubscription<List<int>>? charSubscription;
+      final StreamController<List<int>> savedDataStreamController =
+          StreamController();
+
+      CborDecoder().bind(savedDataStreamController.stream).listen(
+        (value) {
+          Uint8Buffer object = value.toObject() as Uint8Buffer;
+          final dataPayload = decoder.decodePayload(object);
+          measureList.add(dataPayload);
+          downloaded += 1;
+          _loadingPercent = downloaded / totalLength * 100;
+          notifyListeners();
+        },
+        onDone: () async {
+          await payloadChar.setNotifyValue(false);
+          charSubscription?.cancel();
+          await device.disconnect();
+          // for (final data in measureList) {
+          //   log(data.toString());
+          // }
+        },
+      );
+
+      charSubscription = payloadChar.onValueReceived.listen(
+        (payload) async {
+          if (payload.isEmpty) {
+            savedDataStreamController.close();
+            return;
+          }
+          savedDataStreamController.sink.add(payload);
+        },
+        onDone: () {
+          print("charSubscription DONE!");
+        },
+      );
+    }
   }
 
   Future<void> _checkPermissions() async {
--- lib/features/train/presentation/widgets/end_train_widgets/end_train_widget.dart
+++ lib/features/train/presentation/widgets/end_train_widgets/end_train_widget.dart
@@ -2,7 +2,9 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:inmotion_mobile_test/core/presentation/app_container.dart';
+import 'package:inmotion_mobile_test/features/train/presentation/provider/train_model.dart';
 import 'package:inmotion_mobile_test/features/train/presentation/widgets/end_train_widgets/loading_button.dart';
+import 'package:provider/provider.dart';
 
 class EndTrainWidget extends StatelessWidget {
   const EndTrainWidget({super.key});
@@ -10,41 +12,55 @@ class EndTrainWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final model = context.read<TrainModel>();
     return AppContainer(
       padding: const EdgeInsets.all(16),
       width: double.infinity,
       borderRadius: BorderRadius.circular(16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Text("Тренировка завершена", style: theme.textTheme.titleMedium),
-          const SizedBox(height: 10),
-          Text(
-            "Отчет о тренировке формируется, это может занять некоторое время. Пожалуйста не выключайте устройство и не закрывайте окно приложения",
-            style: theme.textTheme.displaySmall,
-          ),
-          const SizedBox(height: 10),
-          LoadingButton(
-            padding: const EdgeInsets.all(8),
-            progressColor: theme.colorScheme.secondaryContainer,
-            backgroundColor:
-                theme.colorScheme.secondaryContainer.withOpacity(0.5),
-            borderRadius: BorderRadius.circular(8),
-            loadingWidget: Text(
-              "Формирование отчета",
-              style: theme.textTheme.labelSmall,
-            ),
-            loadedWidget: Text(
-              "Скачать отчет в Excel",
-              style: theme.textTheme.labelSmall,
-            ),
-            percent: 100,
-            onPressed: () {
-              log("asd");
-            },
-          ),
-          SizedBox(height: 10),
-        ],
+      child: Selector<TrainModel, double>(
+        selector: (context, model) => model.loadingPercent,
+        builder: (context, percent, child) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text("Тренировка завершена", style: theme.textTheme.titleMedium),
+              const SizedBox(height: 10),
+              Text(
+                "Отчет о тренировке формируется, это может занять некоторое время. Пожалуйста не выключайте устройство и не закрывайте окно приложения",
+                style: theme.textTheme.displaySmall,
+              ),
+              const SizedBox(height: 10),
+              LoadingButton(
+                padding: const EdgeInsets.all(8),
+                progressColor: theme.colorScheme.secondaryContainer,
+                backgroundColor:
+                    theme.colorScheme.secondaryContainer.withOpacity(0.5),
+                borderRadius: BorderRadius.circular(8),
+                loadingWidget: Text(
+                  "Формирование отчета",
+                  style: theme.textTheme.labelSmall,
+                ),
+                loadedWidget: Text(
+                  "Скачать отчет в Excel",
+                  style: theme.textTheme.labelSmall,
+                ),
+                percent: percent,
+                onPressed: () {
+                  log("asd");
+                },
+              ),
+              if (percent >= 100) ...[
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    model.prepareRecording();
+                  },
+                  child: Text("back"),
+                ),
+              ]
+            ],
+          );
+        },
       ),
     );
   }
